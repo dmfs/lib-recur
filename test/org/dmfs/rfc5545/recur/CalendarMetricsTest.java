46adfe2865a8e871f0f7d932e0e1143a96e9ba65
Yannic Ahrens <yannic@dmfs.org>
2013-06-25 14:52:00 +0200

    Extracted testcase into CalendarMetricsTest class

---
+++ test/org/dmfs/rfc5545/recur/CalendarMetricsTest.java
@@ -0,0 +1,29 @@
+package org.dmfs.rfc5545.recur;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class CalendarMetricsTest
+{
+	@Test
+	public void testMonthAndDay()
+	{
+		CalendarMetrics tools = new GregorianCalendarMetrics(1, 4);
+		for (int month = 0; month < 12; ++month)
+		{
+			for (int monthday = 1; monthday <= tools.getDaysPerMonth(2000, month); ++monthday)
+			{
+				int mad = CalendarMetrics.monthAndDay(month, monthday);
+
+				int mo = CalendarMetrics.month(mad);
+				int dom = CalendarMetrics.dayOfMonth(mad);
+
+				assertEquals(month, mo);
+				assertEquals(monthday, dom);
+			}
+		}
+	}
+
+}
--- test/org/dmfs/rfc5545/recur/GregorianCalendarMetricsTest.java
+++ test/org/dmfs/rfc5545/recur/GregorianCalendarMetricsTest.java
@@ -372,24 +372,4 @@ public class GregorianCalendarMetricsTest
 		}
 	}
 
-
-	@Test
-	public void testMonthAndDay()
-	{
-		CalendarMetrics tools = new GregorianCalendarMetrics(1, 4);
-		for (int month = 0; month < 12; ++month)
-		{
-			for (int monthday = 1; monthday <= tools.getDaysPerMonth(2000, month); ++monthday)
-			{
-				int mad = CalendarMetrics.monthAndDay(month, monthday);
-
-				int mo = CalendarMetrics.month(mad);
-				int dom = CalendarMetrics.dayOfMonth(mad);
-
-				assertEquals(month, mo);
-				assertEquals(monthday, dom);
-			}
-		}
-	}
-
 }
