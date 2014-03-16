a06f33f822c9fd06939a1a8f65c29bae7ae1e98a
Marten Gajda <marten@dmfs.org>
2014-03-16 20:20:31 +0100

    fix java doc

--- src/org/dmfs/rfc5545/recur/Limiter.java
+++ src/org/dmfs/rfc5545/recur/Limiter.java
@@ -18,7 +18,7 @@
 package org.dmfs.rfc5545.recur;
 
 /**
- * Limits the instance set of a recurrence rules. The only subclasses are {@link CountLimiter} and {@link UntilLimiter} that limit by instance count or by last
+ * Limits the instance set of a recurrence rule. The only subclasses are {@link CountLimiter} and {@link UntilLimiter} which limit by instance count or by last
  * recurrence date respectively.
  * <p>
  * A limiter must always be the last iterator in the chain of iterators.
